51425729e5f66137710a29b29d568c07513e10f3
Grant Davis <grantdavis303@gmail.com>
2024-06-01 13:15:36 -0600

    Add Tests for Welcome Index

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Feature Test", type: :feature do
-  it "individual test" do
+RSpec.describe "Welcome Index Page", type: :feature do
+  it "has button to sign into GitHub" do
+    visit root_path
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Welcome to The Sunken Isles First Mate Application!")
+    expect(page).to have_link("Sign in with GitHub")
+  end
+
+  it "redirects to GitHub OAuth after clicking on the GitHub link" do
+    visit root_path
+
+    click_on("Sign in with GitHub")
+
+    expect(current_path).to eq("/login/oauth/authorize")
   end
 end
-
-# json_response = File.read("") # url to json file
-# stub_request(:get, "https://api.themoviedb.org/3/trending/movie/week?api_key=0f7ff543b9146c27bb69c85b227e5f63").to_return(status: 200, body: json_response)
