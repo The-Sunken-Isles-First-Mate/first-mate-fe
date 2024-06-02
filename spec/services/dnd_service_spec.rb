7e04ab0705a27d9f951b24f8db742be8f7d7bb1a
Logan Finnegan <logantfinnegan@protonmail.com>
2024-06-02 01:16:27 -0500

    service and testing

---
+++ spec/services/dnd_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe DndService do
+  it 'can make API call to database' do
+    query = DndService.call_db('classes')
+    result = query[:results]
+# require 'pry'; binding.pry
+    expect(query).to be_an Hash
+    expect(result).to be_a Array
+    check_hash_structure(result.first, :name, String)
+  end
+
+  it 'can make API call to database' do
+    query = DndService.call_db('races')
+    result = query[:results]
+    
+    expect(query).to be_an Hash
+    expect(result).to be_a Array
+    check_hash_structure(result.first, :name, String)
+  end
+end
