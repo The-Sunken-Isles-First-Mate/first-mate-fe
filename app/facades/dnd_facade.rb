dbfcb5a5530c1f8f427c0893107e7b4f8628a1fe
Grant Davis <grantdavis303@gmail.com>
2024-06-02 18:26:32 -0600

    Update Facades

--- app/facades/dnd_facade.rb
+++ app/facades/dnd_facade.rb
@@ -1,8 +1,6 @@
 class DndFacade 
   def self.classes
     response = DndService.call_db('classes')
-    # dndrace = DndService.call_db('races')
-    # response[:race] = dndrace
     response[:results].map { |r| DndClass.new(r) }
   end
 
