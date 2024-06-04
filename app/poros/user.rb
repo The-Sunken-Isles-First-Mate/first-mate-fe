07492b01b1260cb0c3c261704f6049a35eb9ef23
Grant Davis <grantdavis303@gmail.com>
2024-06-04 15:36:31 -0600

    Create User PORO

---
+++ app/poros/user.rb
@@ -0,0 +1,11 @@
+class User
+  attr_reader :id,
+              :uid,
+              :username
+
+  def initialize(data)
+    @id = data[:id]
+    @uid = data[:uid]
+    @username = data[:username]
+  end
+end
