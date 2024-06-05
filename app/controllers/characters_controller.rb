ade098d28c818950dbaa8386d4aacbfc480ffa5d
Logan Finnegan <logantfinnegan@protonmail.com>
2024-06-05 10:15:44 -0500

    added api caching, refactored dnd api, updated tests

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,7 +1,11 @@
 class CharactersController < ApplicationController
   def new
-    @classes = DndFacade.classes.map  { |race| race.name }
-    @races = DndFacade.races.map { |race| race.name }
+    @attrs = nil
+    Rails.cache.instance_variable_get(:@data).keys.each do |key|
+      @attrs = cached_attrs(key)
+      break if @attrs 
+    end
+    @attrs ||= DndFacade.get_attrs
   end
 
   def create
@@ -18,4 +22,11 @@ class CharactersController < ApplicationController
     # Temp redirect back to dashboard - will go to the summary page
     redirect_to "/dashboard"
   end
+
+  private
+
+  def cached_attrs(key)
+    Rails.cache.fetch(key) if key.include?("attrs_list")
+  end
 end
+
--- spec/facades/dnd_facade_spec.rb
+++ spec/facades/dnd_facade_spec.rb
@@ -3,23 +3,15 @@ require 'rails_helper'
 RSpec.describe DndFacade do
   # VCR.use_cassette('cassette_name.yml') do
     it 'get class info' do
-      result = DndFacade.classes	
-      r      = result.first
-      # require 'pry'; binding.pry
-      expect(result).to be_an(Array)
-      expect(r).to be_a(DndClass)
-      expect(r.name).to be_an(String)
+      result = DndFacade.get_attrs
+      c = result.classes
+      r = result.races
+      
+      expect(result).to be_an(CharsAttr)
+      expect(r).to be_a(Array)
+      expect(c).to be_a(Array)
+      expect(r[0]).to be_an(String)
+      expect(c[0]).to be_an(String)
     end
     # end
-
-  # VCR.use_cassette('cassette_name.yml') do
-    it 'get race info' do
-      result = DndFacade.races	
-      r      = result.first
-      
-      expect(result).to be_an(Array)
-      expect(r).to be_a(DndRace)
-      expect(r.name).to be_an(String)
-    end
-  # end
 end
