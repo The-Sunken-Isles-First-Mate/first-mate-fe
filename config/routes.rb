f81d4fa34c4c95b43acd94271cb54d51a78a0944
Grant Davis <grantdavis303@gmail.com>
2024-06-06 16:58:12 -0600

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   resources :campaigns, only: [:new, :create] do
     get '/summary', to: 'summary#show'
     get '/management', to: 'management#edit'
+    post '/management/advance', to: 'management#advance_week'
     patch '/management/update', to: 'management#update'
-    
+
     #post '/advance_week', to: 'management#advance_week'
   end
 
