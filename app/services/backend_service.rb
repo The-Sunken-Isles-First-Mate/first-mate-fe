66c48289f3611d6edf0b614db17291293c7af319
Grant Davis <grantdavis303@gmail.com>
2024-06-06 16:52:39 -0600

    Update BE Service

--- app/services/backend_service.rb
+++ app/services/backend_service.rb
@@ -104,15 +104,62 @@ class BackendService
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  ### Not formatted
+  def self.post_db_advance_week(campaign_id, management_form)
+    response = connection.post("api/v1/campaigns/#{campaign_id}/advance_week") do |request|
+      request.body = 
+      {
+        campaign: {
+          campaign_id: campaign_id,
+          week: management_form.week,
+          animal_products: management_form.animal_products,
+          cloth: management_form.cloth,
+          farmed_goods: management_form.farmed_goods,
+          food: management_form.food,
+          foraged_goods: management_form.foraged_goods,
+          metal: management_form.metal,
+          monster_parts: management_form.monster_parts,
+          stone: management_form.stone,
+          wood: management_form.wood,
+          light_armor: management_form.light_armor,
+          medium_armor: management_form.medium_armor,
+          heavy_armor: management_form.heavy_armor,
+          simple_weapon: management_form.simple_weapon,
+          martial_weapon: management_form.martial_weapon,
+          ammunition: management_form.martial_weapon,
+          adventuring_supplies: management_form.adventuring_supplies,
+          assassins_blood: management_form.assassins_blood,
+          malice: management_form.malice,
+          midnight_tears: management_form.midnight_tears,
+          serpent_venom: management_form.serpent_venom,
+          truth_serum: management_form.truth_serum,
+          oil_of_slipperiness: management_form.oil_of_slipperiness,
+          potion_of_climbing: management_form.potion_of_climbing,
+          potion_of_healing: management_form.potion_of_healing,
+          potion_of_water_breathing: management_form.potion_of_water_breathing,
+          barge: management_form.barge,
+          coracle: management_form.coracle,
+          double_hulled_sailing_canoe: management_form.double_hulled_sailing_canoe,
+          keelboat: management_form.keelboat,
+          raft: management_form.raft,
+          single_hulled_sailing_canoe: management_form.single_hulled_sailing_canoe,
+          ballista: management_form.ballista,
+          cabin: management_form.cabin,
+          magical_defenses: management_form.magical_defenses,
+          storage: management_form.storage
+        }
+      }.to_json
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.call_db(url)
     response = connection.get(url) do |request|
     end
     JSON.parse(response.body, symbolize_names: true)
   end
-  ###
 
   private
+  
   def self.connection # Replace with hosted database once established
     Faraday.new(
       url: "http://localhost:3000/",
