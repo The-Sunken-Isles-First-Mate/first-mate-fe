ca54e9cfee19cb6cc645d7315e19ead826899a46
Grant Davis <grantdavis303@gmail.com>
2024-06-24 14:41:53 -0600

    Add Sessions Create and Destroy Methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,36 +16,22 @@ class SessionsController < ApplicationController
     redirect_to dashboard_path
   end
 
-  # def create
-  #   client_id = "Ov23lisDr3te8NGMI0MW"
-  #   client_secret = "4638256eea5ccaa7162bd896d521fed3af181509"
-  #   code = params[:code]
+  def create
+    binding.pry
 
-  #   conn = Faraday.new(url: 'https://github.com', headers: {'Accept': 'application/json'})
-  #   response = conn.post('/login/oauth/access_token') do |req|
-  #     req.params['code'] = code
-  #     req.params['client_id'] = client_id
-  #     req.params['client_secret'] = client_secret
-  #   end
-  #   data = JSON.parse(response.body, symbolize_names: true)
+    # user = BackendFacade.get_user({
+    #   username: params[:username],
+    #   password: params[:password],
+    # })
 
-  #   access_token = data[:access_token]
+    # session[:user] = user
 
-  #   conn = Faraday.new(url: 'https://api.github.com', headers: {'Authorization': "token #{access_token}"})
-  #   response = conn.get('/user')
-  #   data = JSON.parse(response.body, symbolize_names: true)
-
-  #   user = BackendFacade.get_user({
-  #     uid: data[:id],
-  #     username: data[:login],
-  #     token: access_token
-  #   })
-
-  #   session[:user] = user
-
-  #   redirect_to dashboard_path
-  # end
+    # redirect_to dashboard_path
+  end
 
   def destroy
+    reset_session
+
+    redirect_to root_path
   end
 end
