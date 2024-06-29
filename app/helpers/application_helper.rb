1f56028d2a7416b5724376ef2f36023bd1903e19
Billy Wallace <wallace.billy1@gmail.com>
2024-06-29 10:24:13 -0600

    Update dnd race and class fields with complete list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def dnd_races
+    [
+      "Aarakocra", "Aasimar", "Bugbear", "Decapodian", "Dragonborn", "Drow", "Dwarf", "Elf", "Eladrin",
+      "Firbolg", "Genasi", "Gnome", "Goblin", "Goliath", "Half-Elf", "Half-Orc", "Halfling",
+      "Hobgoblin", "Human", "Kia'i", "Kenku", "Kobold", "Lizardfolk", "Mant-i", "Mirescale", "Orc",
+      "Svirfneblin", "Tabaxi", "Tiefling", "Triton", "Yuan-Ti Pureblood"
+    ]
+  end
+
+  def dnd_classes
+    [
+      "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", 
+      "Sorcerer", "Warlock", "Wizard"
+    ]
+  end
 end
